f8d1648b3d6f698e4e554e99c5d058aaba97656b
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-10-31 16:11:28 -0400

    Adding form validation to the front page. Also centering it both vertically and horizontally.

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -2,58 +2,66 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { pushState } from 'redux-router';
 
+/*  http://howtocenterincss.com/ */
+const styles = {
+  'height': '100%',
+  'display': 'flex',
+  'align-items': 'center',
+  'justify-content': 'center',
+};
+
 @connect(mapReduxStateToProps)
 export default class LandingPage extends Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
   };
 
-  /*
-   *  Placing the state inside redux state causes a
-   *  huge performance drain.
-   */
-  state = {
-    region: 'na',
-    summonerName: '',
-  };
-
   render() {
-    const { region } = this.state;
     return (
-      <form onSubmit={::this.transitionToSummonerPage}>
-        <input
-          type="text"
-          placeholder="Summoner Name"
-          onChange={::this.updateSummonerName}
-        />
-        <select value={region} onChange={::this.updateRegion}>
-          <option value="na">North America</option>
-          <option value="euw">Europe West</option>
-          <option value="eue">Europe East</option>
-          <option value="kr">Korea</option>
-          <option value="ch">China</option>
-        </select>
-        <button type="submit">Search</button>
-      </form>
+        <form
+          style={styles}
+          onSubmit={::this.transitionToSummonerPage}
+        >
+          <select ref="region">
+            <option value="na">North America</option>
+            <option value="euw">Europe West</option>
+            <option value="eue">Europe East</option>
+            <option value="kr">Korea</option>
+            <option value="ch">China</option>
+          </select>
+          <label>
+            <span className="hide">Summoner Name</span>
+            <input
+              type="text"
+              placeholder="Summoner Name"
+              ref="summonerName"
+              required
+            />
+          </label>
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </form>
     );
-  }
-
-  updateRegion(event) {
-    event.preventDefault();
-    this.setState({ region: event.target.value });
-  }
-
-  updateSummonerName(event) {
-    event.preventDefault();
-    this.setState({ summonerName: event.target.value });
   }
 
   transitionToSummonerPage(event) {
     event.preventDefault();
     const { dispatch } = this.props;
-    const { region, summonerName } = this.state;
-    dispatch(pushState(null, `/${region}/summoners/${summonerName}`));
+    const summonerName = React.findDOMNode(this.refs.summonerName).value;
+    const region = React.findDOMNode(this.refs.region).value;
+
+    if (this.isValidSummonerName(summonerName)) {
+      dispatch(pushState(null, `/${region}/summoners/${summonerName}`));
+    } else {
+      alert('Invalid summoner Name!');
+    }
   }
+
+  isValidSummonerName(summonerName) {
+    return summonerName.length <= 16 && summonerName.length >= 3;
+  }
+
 }
 
 function mapReduxStateToProps() {

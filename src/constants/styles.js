75480fc95f9079b8c19565d4a0c2585b08190dcc
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-11-08 15:38:38 -0500

    refactor(): create a SCALE function to standardize css scales

---
+++ src/constants/styles.js
@@ -0,0 +1,9 @@
+export function SCALE(n) {
+  return `${n >= 0 ? fibonacci(n + 2) : 1 / fibonacci(-n + 2)}rem`;
+}
+
+function fibonacci(n) {
+  return (function loop(f1, f2, i) {
+    return i === n ? f1 : loop(f2, f1 + f2, i + 1);
+  })(0, 1, 0);
+}

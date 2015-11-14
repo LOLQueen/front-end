9e6829b6d4205c08b2d21f98a811cc5510747871
Mandish Shah <shahmandish95@gmail.com>
2015-11-14 17:51:55 -0500

    feat(MatchStats): Add creeps total, color coded KDA

--- src/components/MatchStats.js
+++ src/components/MatchStats.js
@@ -1,6 +1,15 @@
 import React, { PropTypes } from 'react';
 import { Map } from 'immutable';
 
+function threshholdColor(KDA) {
+  if (KDA >= 5) {
+    return 'green';
+  } else if (KDA >= 2) {
+    return 'teal';
+  }
+  return 'gray';
+}
+
 export const MatchStats = ({ data: stats }) => {
   const kills = stats.get('championsKilled', 0);
   const assists = stats.get('assists', 0);
@@ -13,14 +22,18 @@ export const MatchStats = ({ data: stats }) => {
   const level = stats.get('level', 0);
 
   return (
-    <div className="white">
-      <div>
-        <span title={kills}>{kills} /</span>
-        <span title={deaths}> {deaths} /</span>
-        <span title={assists}> {assists}</span>
+    <div className="white h5">
+      <div className="h4">
+        <span>{kills} /</span>
+        <span> {deaths} </span>
+        <span>/ {assists}</span>
       </div>
-      <div className="h5">KDA of <b>{KDA}</b></div>
-      <div className="h5" title={totalCSTitle}>{totalCS} CS</div>
+      <div>
+        KDA: <span className={`bold ${threshholdColor(KDA)}`}>{KDA}</span>
+      </div>
+      <div title={totalCSTitle}>
+        CS: <span className="bold">{totalCS}</span>
+      </div>
     </div>
   );
 };

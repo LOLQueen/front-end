896da127afc60ecf00a673998fd1254315dfeb75
Mandish Shah <shahmandish95@gmail.com>
2015-11-14 16:56:50 -0500

    feat(MatchStats): add a creep total to display total CS

--- src/components/MatchStats.js
+++ src/components/MatchStats.js
@@ -6,13 +6,21 @@ export const MatchStats = ({ data: stats }) => {
   const assists = stats.get('assists', 0);
   const deaths = stats.get('numDeaths', 0);
   const KDA = ((kills + assists) / (deaths || 1)).toFixed(2);
+  const minionsKilled = stats.get('minionsKilled', 0);
+  const neutralMinionsKilled = stats.get('neutralMinionsKilled', 0);
+  const totalCS = minionsKilled + neutralMinionsKilled;
+  const totalCSTitle = `${minionsKilled} Minions + ${neutralMinionsKilled} Jungle Creeps`;
+  const level = stats.get('level', 0);
 
   return (
     <div className="white">
+      <div>
         <span title={kills}>{kills} /</span>
         <span title={deaths}> {deaths} /</span>
-        <span title={assists}> {assists}</span> <br/>
-        <span>KDA of <b>{KDA}</b></span>
+        <span title={assists}> {assists}</span>
+      </div>
+      <div className="h5">KDA of <b>{KDA}</b></div>
+      <div className="h5" title={totalCSTitle}>{totalCS} CS</div>
     </div>
   );
 };

12cda3041f827051604960bc686cd56f4f0e1af1
Mandish Shah <shahmandish95@gmail.com>
2015-11-18 21:01:13 -0500

    feat((PlayerList)): adding a generic style to both teams & also to individual players

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -6,22 +6,37 @@ const PlayerList = ({ data: players }) => {
   const purpleTeam = players.get('purple', List());
 
   const bluePlayersJSX = blueTeam.map((player, index) => (
-    <div key={index}>{player.getIn(['summoner', 'name'], 'N/A')}</div>
+    <div style={styles.playerName} key={index}>{player.getIn(['summoner', 'name'], 'N/A')}</div>
   ));
   const purplePlayersJSX = purpleTeam.map((player, index) => (
-    <div key={index}>{player.getIn(['summoner', 'name'], 'N/A')}</div>
+    <div style={styles.playerName} key={index}>{player.getIn(['summoner', 'name'], 'N/A')}</div>
   ));
 
   return (
-    <div className="flex h6">
-      <div>
+    <div className="flex h6 white" style={styles.teams}>
+      <div className="px1" style={styles.blueTeam}>
         {bluePlayersJSX}
       </div>
-      <div>
+      <div className="px1" style={styles.purpleTeam}>
         {purplePlayersJSX}
       </div>
     </div>
   );
+};
+
+const styles = {
+  playerName: {
+    width: '75px',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+  },
+  teams: {
+  },
+  blueTeam: {
+  },
+  purpleTeam: {
+  },
 };
 
 PlayerList.propTypes = {
